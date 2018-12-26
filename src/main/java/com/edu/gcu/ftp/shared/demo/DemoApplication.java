d834210456ed4d10d8059bbe0932f2bd7f9356dc
tr-y <3551222684@qq.com>
2018-12-26 22:20:13 +0800

    Base

---
+++ src/main/java/com/edu/gcu/ftp/shared/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.edu.gcu.ftp.shared.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
+
+}
+
